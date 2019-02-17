74203edfadf36cb7af3aab4e23681df2c6728d9c
DenilsonDesigns <daniellanguilller@gmail.com>
2019-02-17 17:53:15 +1100

    started hbs

--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const path = require("path");
 
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
