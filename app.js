9dba9e7e88c672971d861056235e775197b9b189
DenilsonDesigns <daniellanguilller@gmail.com>
2019-02-17 21:24:43 +1100

    finished templating engines

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
-const expressHbs = require("express-handlebars");
-
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.engine("hbs", expressHbs());
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.routes);
