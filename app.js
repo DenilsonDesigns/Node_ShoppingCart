f1b20d3f18f357d756dff76bc21ad3f9c95b15d0
DenilsonDesigns <daniellanguilller@gmail.com>
2019-03-02 20:41:31 +1100

    sessions added

--- app.js
+++ app.js
@@ -3,20 +3,37 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const errorController = require("./controllers/error");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
+const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI = "mongodb://localhost:27017/store";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions"
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("5c7899ff1a0e091d40f2de88")
@@ -29,18 +46,19 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect("mongodb://localhost:27017/store", { useNewUrlParser: true })
-  .then(() => {
-    console.log("Connected to Mongoose");
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(result => {
+    console.log("Mongo Connected");
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: "Doge",
-          email: "doge69@doge.com",
+          name: "Max",
+          email: "max@test.com",
           cart: {
             items: []
           }
@@ -48,8 +66,9 @@ mongoose
         user.save();
       }
     });
-    app.listen(3000);
-    console.log("Listening on Port: 3000");
+    app.listen(3000, () => {
+      console.log("Listening on PORT:3000");
+    });
   })
   .catch(err => {
     console.log(err);
