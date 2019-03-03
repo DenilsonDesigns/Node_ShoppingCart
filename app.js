8762200ee94b8cbd71bf394a3167a35491abc2da
DenilsonDesigns <daniellanguilller@gmail.com>
2019-03-03 15:57:10 +1100

    finished sessions

--- app.js
+++ app.js
@@ -36,12 +36,17 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById("5c7899ff1a0e091d40f2de88")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.use("/admin", adminRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
   //   const isLoggedIn = req.get("Cookie").split("=")[1] === "true";
   //   console.log(req.session);
@@ -9,6 +11,27 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect("/");
+  User.findById("5c7899ff1a0e091d40f2de88")
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect("/");
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
 };
